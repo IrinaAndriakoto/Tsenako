dea5b126ab1d0f8cb6dcc15d72ffef66c989c5a2
IrinaAndriakoto <119145568+IrinaAndriakoto@users.noreply.github.com>
2026-03-13 20:08:35 +0300

    redo

---
+++ tsenako-backend/src/main/java/tsenako/demo/TsenakoApplication.java
@@ -0,0 +1,13 @@
+package tsenako.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsenakoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsenakoApplication.class, args);
+	}
+
+}
---
+++ tsenako-backend/src/test/java/tsenako/demo/TsenakoApplicationTests.java
@@ -0,0 +1,13 @@
+package tsenako.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsenakoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
